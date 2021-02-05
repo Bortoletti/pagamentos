413be37dd03deb5e16734152a72bb74b98c17e61
Luis Alexandre Bortoletti <lbortoletti@deltasystems.com.br>
2021-02-05 19:20:16 -0300

    Update _setup.php

--- email/_setup.php
+++ email/_setup.php
@@ -1,11 +1,10 @@
 <?php
 
-$setup_Host = 'smtplw.com.br'; // Endereço do servidor SMTP (Autenticação, utilize o host smtp.seudomínio.com.br)
+$setup_Host = 'zzz.com.br'; // Endereço do servidor SMTP (Autenticação, utilize o host smtp.seudomínio.com.br)
 $setup_SMTPAuth   = true;  // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
 $setup_Port       = 587; //  Usar 587 porta SMTP
-$setup_Username = 'deltasystems'; // Usuário do servidor SMTP (endereço de email)
-$setup_Password = 'EKWyHtmn1786'; // Senha do servidor SMTP (senha do email usado)
-//$setup_EmailFrom = 'deltafin@deltasystems.com.br';
-$setup_EmailFrom = 'o2@o2pagamentos.com.br';
-$setup_EmailBCC  = 'suporte@deltasystems.com.br';
+$setup_Username = 'zzz'; // Usuário do servidor SMTP (endereço de email)
+$setup_Password = 'zzz'; // Senha do servidor SMTP (senha do email usado)
+$setup_EmailFrom = 'zz@zz.com.br';
+$setup_EmailBCC  = 'zz@zz.com.br';
 ?>
